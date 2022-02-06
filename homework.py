5787ef178233a4f3aa01f28fc7c6ca5915ab5462
Wingorman <wingorman1307@gmail.com>
2022-02-06 21:47:42 +0800

    creat .env

--- homework.py
+++ homework.py
@@ -1,21 +1,26 @@
-...
+import os
+import requests
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv("practicum_token")
+TELEGRAM_TOKEN = os.getenv("telegram_token")
+TELEGRAM_CHAT_ID = os.getenv("telegram_chat_id")
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
@@ -25,7 +30,7 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+    params = {"from_date": timestamp}
 
     ...
 
@@ -72,12 +77,12 @@ def main():
             time.sleep(RETRY_TIME)
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             ...
             time.sleep(RETRY_TIME)
         else:
             ...
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
